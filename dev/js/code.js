945d9357e4d45fc95d850ee4ad56fb5d1e1fe6e7
00King00 <mr.1bilous@gmail.com>
2017-11-27 16:34:49 +0200

    change serch

--- dev/js/code.js
+++ dev/js/code.js
@@ -1,18 +1,45 @@
 $(function(){
+	// this part for header menu in mobileonly
 	$(".down").click(function(){
 		$(this).hide(300);
 		$(".header_menu ul").slideDown(1000);
 		setTimeout(function(){
-			$(".up").show(300);	
+			$(".up").show(300);
 		},1000)
-		
 	})
+	
 	$(".up").click(function(){
 		$(this).hide(300);
 		$(".header_menu ul").slideUp(1000);
 		setTimeout(function(){
 			$(".down").show(300);
 		},700)
+	})
+	
+	// this part for search
+	var count = 0;
+	$(".search img").click(function(){
+		count++;
+		if(count % 2 == 0){
+			$('input[type="search"]').css('display' ,'none')
+		} else{
+			$('input[type="search"]').css({
+			opacity:1,
+			display:'block',
+			height:'38px',
+			width: '200px',
+			'padding-left': '32px',
+			color: '#000',
+			'background-color': '#fff',
+			'cursor': 'auto',
+			'outline': 'none',
+			});
+		}
+		
+		
+		console.log(count);
+		
+		
 		
 	})
 });
