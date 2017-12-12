b94ef442364fb159f6976e10315b4533a4761d7b
Coopert <kupchynskyi@gmail.com>
2017-12-12 21:43:33 +0200

    change file names

--- dev/js/code.js
+++ dev/js/code.js
@@ -159,24 +159,24 @@ $(function(){
 
 	//this part for rating
 	
-	$("#rating").mousemove(function(e){
+	$(".rating").mousemove(function(e){
 		if($(".testimonials__rating").hasClass("rated")){
 			return false;
 		}
-		var offset = $("#rating").offset(), coords = e.clientX - offset.left;
+		var offset = $(".rating").offset(), coords = e.clientX - offset.left;
 		var stars = Math.round((coords+6.4)/12.8);
 		var stars_value =stars*10 + "%";
-		$("#rating-hover").css("width",stars_value);
+		$(".rating-hover").css("width",stars_value);
 	});
 	
 	$("#rating-hover").mousemove(function(e){
 		if($(".testimonials__rating").hasClass("rated")){
 			return false;
 		}
-		var offset = $("#rating").offset(), coords = e.clientX - offset.left;
+		var offset = $(".rating").offset(), coords = e.clientX - offset.left;
 		var stars = Math.round((coords+6.4)/12.8);
 		var stars_value =stars*10 + "%";
-		$("#rating-hover").css("width",stars_value);
+		$(".rating-hover").css("width",stars_value);
 	
 	});
 	
@@ -184,14 +184,14 @@ $(function(){
 		if($(this).hasClass("rated")){
 			return false;
 		}
-		$("#rating-hover").css("width",0);
+		$(".rating-hover").css("width",0);
 	});
 		
 	$(".testimonials__rating").click(function(e){
-		var offset = $("#rating").offset(), coords = e.clientX - offset.left;
+		var offset = $(".rating").offset(), coords = e.clientX - offset.left;
 		var stars = Math.round((coords+6.4)/12.8);
 		var stars_value =stars*10 + "%";
-		$("#rating-hover").css("width",stars_value)
+		$(".rating-hover").css("width",stars_value)
 		$(this).addClass("rated")
 		
 		return
